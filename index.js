d22535d76a18aff3977e7ba9667c8a7cdaac8194
Daniela Carey <daniela@winnie.com>
2019-10-10 14:54:11 -0700

    Eat pizza

--- index.js
+++ index.js
@@ -10,12 +10,24 @@ const app = express();
 // the port used for express server
 let PORT = process.env.PORT || 3000
 
+const findPizza = (date) => {
+  // scrape arizmendi website to get find that date
+  // http://arizmendi-valencia.squarespace.com/pizza/
+}
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('hello world')
+
+  // let date = get current day of the week / date
+  
+  // let pizza = findPizza(date)
+ 
+  // send json response in slack format
 })
+
 
 // start server
 app.listen(PORT, () => {
